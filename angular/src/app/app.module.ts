21bc0ef448a5fc661f9e54d5e2d16aed52f5ded3
Bjim <imbjim1@gmail.com>
2017-05-19 17:07:53 +0200

    components added

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,10 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    LoginComponent,
+    DashboardComponent,
+    ProfileComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
