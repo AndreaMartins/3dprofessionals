86db3bfca5773244f422733874e53adbcea32e3e
Andrea Martins <marlit8@gmail.com>
2017-05-22 16:11:17 +0200

    authentication

--- 3dprofessionals-api/app.js
+++ 3dprofessionals-api/app.js
@@ -4,15 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var auth = require('./routes/auth');
 
 var app = express();
 
+mongoose.connect(`mongodb://localhost:27017/3dprofessionals`);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,6 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/', auth);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
