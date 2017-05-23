d5626a0b3449f07d8872f6f8e338883ae1202199
Bjim <imbjim1@gmail.com>
2017-05-23 11:18:34 +0200

    signup and login working

---
+++ angular/src/app/navbar/navbar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../services/session.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(
+
+    private session: SessionService,
+  	private router:  Router,
+  ) { }
+
+  ngOnInit() {
+  }
+
+  logout() {
+  this.session.logout();
+  // this.router.navigate(['/login']);
+}
+
+}
