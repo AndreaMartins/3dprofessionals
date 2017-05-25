b899a6ccd32c0779f357764621facee01462a9bc
Andrea Martins <marlit8@gmail.com>
2017-05-25 10:41:10 +0200

    update

--- angular/src/app/app.routing.ts
+++ angular/src/app/app.routing.ts
@@ -17,11 +17,11 @@ export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'dashboard', component: DashboardComponent /* canActivate: [SessionService] */ },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [SessionService] },
+  { path: 'profile', component: ProfileComponent, canActivate: [SessionService] },
 
-  { path: 'project/:id', component: ProjectComponent },
+  { path: 'project/:id', component: ProjectComponent, canActivate: [SessionService] },
 
-  { path: 'editprofile', component: EditprofileComponent }
+  { path: 'editprofile', component: EditprofileComponent, canActivate: [SessionService] }
 
 ];
